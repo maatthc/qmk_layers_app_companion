937b9ea63e3e14ee9d506240812fb065d0cd286c
Alexandre Andrade <aleandrade@gmail.com>
2025-02-13 15:39:47 +1100

    refactor: 💡 Use Kivy instead

--- keyboard_layers.py
+++ keyboard_layers.py
@@ -1,49 +1,28 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.image import AsyncImage
 import keyboard
-import tkinter as tk
-from PIL import Image, ImageTk
-
-def on_function_key(layer):
-    print('Layer:', layer)
-    app.updateImage(layer)
 
 imageFolder = './assets/'
 
-class Resizable:
-    def __init__(self, master):
-        self.master = master
-        self.master.geometry("600x400")
-        self.master.title("Miryoku Layouts")
-        self.growCanvas = tk.Canvas(self.master)
-        self.growCanvas.pack(fill = tk.BOTH, expand = tk.YES)
-        picture = imageFolder + 'miryoku-kle-base.png'
-        self.image = Image.open(picture)
-        self.image_copy = self.image.copy()
-        self.background = ImageTk.PhotoImage(self.image)
-        self.label = tk.Label(
-            self.growCanvas, image = self.background, borderwidth = 0)
-        self.label.bind('<Configure>', self.resizeImage)
-        self.label.pack(fill = tk.BOTH, expand = tk.YES, padx = 2, pady = 2)
-    def resizeImage(self, event):
-        image = self.image_copy.resize(
-            (self.master.winfo_width(), self.master.winfo_height()))
-        self.newimage = ImageTk.PhotoImage(image)
-        self.label.config(image = self.newimage)
-    def updateImage(self, picture):
-        self.image = Image.open(imageFolder + 'miryoku-kle-' + picture + '.png')
-        self.image_copy = self.image.copy()
-        self.resizeImage(None)
+class MyApp(App):
+  def build(self):
+    self.title = 'Miryoku Layouts'
+    self.img = AsyncImage(source= imageFolder + 'miryoku-kle-base.png',  allow_stretch=True)
+    return self.img
 
-try:
-    app = Resizable(tk.Tk())
+  def on_function_key(self, layer):
+      print('Layer:', layer)
+      self.img.source = imageFolder + 'miryoku-kle-' + layer + '.png'
+  
+app = MyApp()
+keyboard.add_hotkey(183, lambda: app.on_function_key('base'))
+keyboard.add_hotkey(184, lambda: app.on_function_key('nav'))
+keyboard.add_hotkey(185, lambda: app.on_function_key('mouse'))
+keyboard.add_hotkey(186, lambda: app.on_function_key('media'))
+keyboard.add_hotkey(187, lambda: app.on_function_key('num'))
+keyboard.add_hotkey(188, lambda: app.on_function_key('sym'))
+keyboard.add_hotkey(189, lambda: app.on_function_key('fun'))
 
-    keyboard.add_hotkey(183, lambda: on_function_key('base'))
-    keyboard.add_hotkey(184, lambda: on_function_key('nav'))
-    keyboard.add_hotkey(185, lambda: on_function_key('mouse'))
-    keyboard.add_hotkey(186, lambda: on_function_key('media'))
-    keyboard.add_hotkey(187, lambda: on_function_key('num'))
-    keyboard.add_hotkey(188, lambda: on_function_key('sym'))
-    keyboard.add_hotkey(189, lambda: on_function_key('fun'))
-
-    app.master.mainloop()
-except Exception as error:
-    print("An error occurred:", type(error).__name__, error)
+app.run()
