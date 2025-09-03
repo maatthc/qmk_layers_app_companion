a55b4ce0dc29d8a7f406a6dc3272f5216a768981
Alexandre Andrade <aleandrade@gmail.com>
2025-09-03 14:26:11 -0300

    refactor: 💡 Close grafully

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import sys
 import os
 os.environ["KIVY_NO_ARGS"] = "1"
 
@@ -9,6 +10,13 @@ from kivy.uix.image import AsyncImage
 imageFolder = './assets/'
 
 class Gui(App):
+  def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.thread_event=None
+
+  def set_thread_event(self,thr):
+        self.thread_event=thr
+
   def build(self):
     self.title = 'Keyboard : Miryoku Layouts'
     self.img = AsyncImage(source= imageFolder + 'miryoku-kle-base.png',  allow_stretch=True)
@@ -16,4 +24,9 @@ class Gui(App):
 
   def on_function_key(self, layer):
       self.img.source = imageFolder + 'miryoku-kle-' + layer + '.png'
-  
+
+  def on_stop(self):
+        print('App closing..')
+        if self.thread_event is not None:
+            self.thread_event.set()
+            sys.exit()
