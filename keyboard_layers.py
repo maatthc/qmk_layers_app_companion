285b6d6d8e10be1143394b23515be416f19b175c
Alexandre Andrade <aleandrade@gmail.com>
2025-09-03 12:44:22 -0300

    refactor: 💡 Add support to remote screen

--- keyboard_layers.py
+++ keyboard_layers.py
@@ -1,28 +1,32 @@
-import kivy
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.image import AsyncImage
-import keyboard
+from gui import Gui
+from sender import Sender
+from client import Client
+from args import parser
+from hotkeys import key_listener
 
-imageFolder = './assets/'
+gui = None
+sender = None
 
-class MyApp(App):
-  def build(self):
-    self.title = 'Miryoku Layouts'
-    self.img = AsyncImage(source= imageFolder + 'miryoku-kle-base.png',  allow_stretch=True)
-    return self.img
+args = parser()
 
-  def on_function_key(self, layer):
-      print('Layer:', layer)
-      self.img.source = imageFolder + 'miryoku-kle-' + layer + '.png'
-  
-app = MyApp()
-keyboard.add_hotkey(183, lambda: app.on_function_key('base'))
-keyboard.add_hotkey(184, lambda: app.on_function_key('nav'))
-keyboard.add_hotkey(185, lambda: app.on_function_key('mouse'))
-keyboard.add_hotkey(186, lambda: app.on_function_key('media'))
-keyboard.add_hotkey(187, lambda: app.on_function_key('num'))
-keyboard.add_hotkey(188, lambda: app.on_function_key('sym'))
-keyboard.add_hotkey(189, lambda: app.on_function_key('fun'))
+def send_keystroke(key):
+    if sender is not None:
+        sender.notify(key)
+    else:
+        gui.on_function_key(key)
 
-app.run()
+if args.client:
+    gui = Gui()
+    Client(gui)
+    gui.run()
+
+else:
+    key_listener(send_keystroke)
+
+    if args.remote:
+        sender = Sender()
+        sender.connect()
+        keyboard.wait()
+    else:
+        gui = Gui()
+        gui.run()
