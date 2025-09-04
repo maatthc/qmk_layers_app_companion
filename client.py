8267b0d7c7168453929c764c2aabfb832728ebcc
Alexandre Andrade <aleandrade@gmail.com>
2025-09-04 12:40:13 -0300

    feat: 🎸 Manage recv timeout

--- client.py
+++ client.py
@@ -13,19 +13,31 @@ class Client:
         self.socket.bind((host, port))
         print(f'Binding to : {host}:{port}')
         self.socket.listen(NUM_CLIENTS)
+        self.socket.settimeout(1)
         event=threading.Event()
-        thread = threading.Thread(target=self.serve,args=(event)) 
+        thread = threading.Thread(target=self.serve,args=(event,)) 
+        thread.deamon = True
         thread.start()
         self.gui.set_thread_event(event)
 
     def serve(self, event):
         while not event.is_set():
-            conn, address = self.socket.accept()
-            print("Connection from: " + str(address))
-            while True:
-                data = str(conn.recv(1024).decode())
-                if not data:
-                    break
-                print("Data received: " + str(data))
-                self.gui.on_function_key(data) 
-            conn.close()
+            try:
+                conn, address = self.socket.accept()
+                conn.settimeout(1)
+                print("Connection from: " + str(address))
+                while not event.is_set():
+                    try:
+                        data = str(conn.recv(1024).decode())
+                        if not data:
+                            break
+                        print("Data received: " + str(data))
+                        self.gui.on_function_key(data) 
+                    except:
+                        continue
+                conn.close()
+            except socket.timeout:
+                continue
+            except Exception as e:
+                print(f"Error with client {address}: {e}")
+        self.socket.close()
