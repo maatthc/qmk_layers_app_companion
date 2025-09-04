29dd26dda361e00c7cd35fb276306de6b9f60d19
Alexandre Andrade <aleandrade@gmail.com>
2025-09-04 13:26:15 -0300

    feat: 🎸 Add client port

--- args.py
+++ args.py
@@ -1,16 +1,38 @@
 import argparse
 
+
 def parser():
     parser = argparse.ArgumentParser(description="Keyboard companion gui")
-    parser.add_argument('-r', '--remote', action='store_true', help='Send keystrokes to remote host that will display Keyboard layout')
-    parser.add_argument('-i', '--client_ip', action='store_true', help='IP address of the remote host - if not provided, local network discovery will be used.')
-    parser.add_argument('-c', '--client', action='store_true', help='Receives keystrokes from remote host to display Keyboard layout')
+    parser.add_argument(
+        "-r",
+        "--remote",
+        action="store_true",
+        help="Send keystrokes to remote host that will display Keyboard layout",
+    )
+    parser.add_argument(
+        "-i",
+        "--client_ip",
+        help="IP address of the remote host - if not provided, local network discovery will be used.",
+    )
+    parser.add_argument(
+        "-p",
+        "--client_port",
+        type=int,
+        default=1977,
+        help="Port to be used by the client - Default : 1977",
+    )
+    parser.add_argument(
+        "-c",
+        "--client",
+        action="store_true",
+        help="Receives keystrokes from remote host to display Keyboard layout",
+    )
     args = parser.parse_args()
 
     if args.client_ip and not args.remote:
-        parser.error('--client_ip requires --remote')
+        parser.error("--client_ip requires --remote")
 
-    if args.client and  args.remote:
-        parser.error('Use --client or --remote')
+    if args.client and args.remote:
+        parser.error("Use --client or --remote")
 
     return args
