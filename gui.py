82696746df776b413692fdd4a5c9500b54f56ded
Alexandre Andrade <aleandrade@gmail.com>
2025-09-12 13:52:50 -0300

    feat: 🎸 Allow layer image configuration

--- gui.py
+++ gui.py
@@ -1,20 +1,19 @@
 import os
+from config import Config
 
 os.environ["KIVY_NO_ARGS"] = "1"
 
-import kivy
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.image import AsyncImage
+from kivy.app import App  # noqa: F401
+from kivy.uix.image import AsyncImage  # noqa: F401
 
 imageFolder = "./assets/"
-imagePrefix = "miryoku-kle-"
 
 
 class Gui(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.thread_event = None
+        self.conf = Config()
 
     def set_thread_event(self, thr):
         self.thread_event = thr
@@ -22,12 +21,13 @@ class Gui(App):
     def build(self):
         self.title = "Keyboard : Miryoku Layouts"
         self.img = AsyncImage(
-            source=imageFolder + imagePrefix + "base.png", allow_stretch=True
+            source=imageFolder + self.conf.layers[0], allow_stretch=True
         )
         return self.img
 
     def on_function_key(self, layer):
-        self.img.source = imageFolder + imagePrefix + layer + ".png"
+        print(f"Switched to layer {layer}: {self.conf.layers[layer]}")
+        self.img.source = imageFolder + self.conf.layers[layer]
 
     def on_stop(self, **kwargs):
         print("App closing..")
