b5d2ebdf19c0f3dae1936785581169df2a9a272d
Alexandre Andrade <aleandrade@gmail.com>
2025-09-12 13:54:32 -0300

    refactor: 💡 Better thread event mngt

--- client.py
+++ client.py
@@ -15,33 +15,37 @@ class Client:
         print(f"Binding to : {host}:{port}")
         self.socket.listen(NUM_CLIENTS)
         self.socket.settimeout(1)
-        event = threading.Event()
-        thread = threading.Thread(target=self.serve, args=(event,))
+        self.event = threading.Event()
+        thread = threading.Thread(target=self.serve)
         thread.deamon = True
         thread.start()
-        self.gui.set_thread_event(event)
+        self.gui.set_thread_event(self.event)
 
         adv = Advertiser(port)
         adv.register()
 
-    def serve(self, event):
-        while not event.is_set():
+    def serve(self):
+        while not self.event.is_set():
             try:
                 conn, address = self.socket.accept()
                 conn.settimeout(1)
                 print("Connection from: " + str(address))
-                while not event.is_set():
+                while not self.event.is_set():
                     try:
                         data = str(conn.recv(1024).decode())
                         if not data:
                             break
-                        print("Data received: " + str(data))
-                        self.gui.on_function_key(data)
-                    except:
+                        self.gui.on_function_key(int(data))
+                    except socket.timeout:
+                        continue
+                    except Exception as e:
+                        print(f"Error receiving data: {e}")
                         continue
                 conn.close()
             except socket.timeout:
                 continue
             except Exception as e:
                 print(f"Error with client {address}: {e}")
+
+    def __del__(self):
         self.socket.close()
