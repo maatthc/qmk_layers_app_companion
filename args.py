e94ef2ab27279d67f705fd542ca4e43349aba4f3
Alexandre Andrade <aleandrade@gmail.com>
2025-09-15 22:50:55 -0300

    refactor: 💡 Args for server/client

--- args.py
+++ args.py
@@ -2,37 +2,37 @@ import argparse
 
 
 def parser():
-    parser = argparse.ArgumentParser(description="Keyboard companion gui")
+    parser = argparse.ArgumentParser(description="Keyboard companion app")
     parser.add_argument(
-        "-r",
-        "--remote",
+        "-s",
+        "--server",
         action="store_true",
         help="Send keystrokes to remote host that will display Keyboard layout",
     )
     parser.add_argument(
         "-i",
-        "--client_ip",
-        help="IP address of the remote host - if not provided, local network discovery will be used.",
+        "--server_ip",
+        help="IP address of the server - if not provided, local network discovery will be used.",
     )
     parser.add_argument(
         "-p",
-        "--client_port",
+        "--server_port",
         type=int,
         default=1977,
-        help="Port to be used by the client - Default : 1977",
+        help="Port to be used by the server - Default : 1977",
     )
     parser.add_argument(
         "-c",
         "--client",
         action="store_true",
-        help="Receives keystrokes from remote host to display Keyboard layout",
+        help="Receives keystrokes from remote server to display Keyboard layout",
     )
     args = parser.parse_args()
 
-    if args.client_ip and not args.remote:
-        parser.error("--client_ip requires --remote")
+    if args.server_ip and not args.client:
+        parser.error("--server_ip requires --client")
 
-    if args.client and args.remote:
-        parser.error("Use --client or --remote")
+    if args.client and args.server:
+        parser.error("Use --client or --server, not both")
 
     return args
