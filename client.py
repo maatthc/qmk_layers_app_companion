f3002a05c761edfdf7fb3b1b666d988dd1fd5d7f
Alexandre Andrade <aleandrade@gmail.com>
2025-09-16 10:37:36 -0300

    refactor: 💡 Client connects to remote

--- client.py
+++ client.py
@@ -1,51 +1,51 @@
 import socket
-import threading
-from zeroconfig import Advertiser
-
-NUM_CLIENTS = 1
+from tenacity import retry, wait_exponential
+from zeroconfig import ServiceDiscover
 
 
 class Client:
-    def __init__(self, gui, port):
-        host = "0.0.0.0"
-
+    def __init__(self, gui, args):
         self.gui = gui
+        self.setup()
+
+        if args.server_ip is not None:
+            self.server_ip = args.server_ip
+            self.server_port = args.server_port
+            return
+
+        zero = ServiceDiscover()
+        self.server_ip, self.server_port = zero.find()
+
+    def setup(self):
+        self.retries = 0
+        self.connected = False
         self.socket = socket.socket()
-        self.socket.bind((host, port))
-        print(f"Binding to : {host}:{port}")
-        self.socket.listen(NUM_CLIENTS)
-        self.socket.settimeout(1)
-        self.event = threading.Event()
-        thread = threading.Thread(target=self.serve)
-        thread.deamon = True
-        thread.start()
-        self.gui.set_thread_event(self.event)
 
-        adv = Advertiser(port)
-        adv.register()
+    @retry(wait=wait_exponential(multiplier=1, min=1, max=10))
+    def connect(self):
+        self.retries += 1
+        print(f"Trying to connect to server ... #{self.retries}")
+        self.socket.connect((self.server_ip, self.server_port))
+        self.connected = True
+        print("Connected.")
+        self.receive_changes()
 
-    def serve(self):
-        while not self.event.is_set():
-            try:
-                conn, address = self.socket.accept()
-                conn.settimeout(1)
-                print("Connection from: " + str(address))
-                while not self.event.is_set():
-                    try:
-                        data = str(conn.recv(1024).decode())
-                        if not data:
-                            break
-                        self.gui.on_function_key(int(data))
-                    except socket.timeout:
-                        continue
-                    except Exception as e:
-                        print(f"Error receiving data: {e}")
-                        continue
-                conn.close()
-            except socket.timeout:
-                continue
-            except Exception as e:
-                print(f"Error with client {address}: {e}")
+    def receive_changes(self):
+        if self.connected is True:
+            while True:
+                try:
+                    data = str(self.socket.recv(1024).decode())
+                    if not data:
+                        break
+                    self.gui.updateLayer(int(data))
+                except Exception as e:
+                    print(f"{e} - Error with connection: Reconnecting..")
+                    self.conn_reset()
+
+    def conn_reset(self):
+        self.socket.close()
+        self.setup()
+        self.connect()
 
     def __del__(self):
         self.socket.close()
