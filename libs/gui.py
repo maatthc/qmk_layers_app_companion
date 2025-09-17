69a74d724ff9a676696d88adaa76868c5bf04d8b
Alexandre Andrade <aleandrade@gmail.com>
2025-09-17 10:42:39 -0300

    refactor: 💡 Use listeners

--- libs/gui.py
+++ libs/gui.py
@@ -4,30 +4,28 @@ from libs.config import Config
 os.environ["KIVY_NO_ARGS"] = "1"
 
 from kivy.app import App  # noqa: F401
-from kivy.uix.image import AsyncImage  # noqa: F401
+from kivy.uix.image import Image  # noqa: F401
 from kivy.clock import Clock  # noqa: F401
 
 imageFolder = "./assets/"
 
 
 class Gui(App):
-    def __init__(self, keyboard, **kwargs):
+    def __init__(self, listener, **kwargs):
         super().__init__(**kwargs)
         self.conf = Config()
-        self.keyboard = keyboard
+        self.listener = listener
 
     def on_start(self):
-        Clock.schedule_interval(self.updateLayer, 0.016)  # ~60 FPS
+        Clock.schedule_interval(self.updateLayer, 0.5)
 
     def build(self):
         self.title = "Keyboard Layers App companion"
-        self.img = AsyncImage(
-            source=imageFolder + self.conf.layers[0], allow_stretch=True
-        )
+        self.img = Image(source=imageFolder + self.conf.layers[0], allow_stretch=True)
         return self.img
 
     def updateLayer(self, dt):  # dt is the time since the last call, can be ignored
-        layer = self.keyboard.notify_changes()
+        layer = self.listener.notify_changes()
         if layer is None:
             return
         print(f"Switched to layer {layer}: {self.conf.layers[layer]}")
