4f1630856148d2829b261ac2d0d37f58fcffe027
Alexandre Andrade <aleandrade@gmail.com>
2025-09-17 19:25:47 -0300

    refactor: 💡 Make ZeroConf async

--- libs/zeroconfig.py
+++ libs/zeroconfig.py
@@ -1,10 +1,14 @@
 import socket
-import time
+import asyncio
 from zeroconf import (
     ServiceStateChange,
     ServiceInfo,
-    ServiceBrowser,
     Zeroconf,
+)
+from zeroconf.asyncio import (
+    AsyncServiceBrowser,
+    AsyncServiceInfo,
+    AsyncZeroconf,
 )
 
 service_type = "_keyboard._tcp.local."
@@ -13,6 +17,7 @@ service_name = f"Companion.{service_type}"
 
 class Advertiser:
     def __init__(self, service_port=1977):
+        self.running_task = None
         self.zeroconf = Zeroconf()
         service_properties = {"path": "/", "version": "1.0"}
 
@@ -29,27 +34,39 @@ class Advertiser:
             server=host_name,
         )
 
+    async def registered_task(self):
+        while True:
+            try:
+                await self.zeroconf.async_register_service(self.info)
+            except KeyboardInterrupt:
+                break
+            except Exception:
+                pass
+            await asyncio.sleep(1)
+        print("Exiting Zeroconf background task")
+
     async def register(self):
         print(f"Registering service: {self.info.name}")
-        await self.zeroconf.async_register_service(self.info)
+        self.running_task = asyncio.create_task(self.registered_task())
 
     def __end__(self):
         print("Unregistering service...")
         self.zeroconf.unregister_service(self.info)
         self.zeroconf.close()
+        if self.running_task is not None:
+            self.running_task.cancel()
 
 
 class ServiceDiscover:
     def __init__(self):
         self.service_info = None
         print("Searching for Companion App server..")
-        self.zc = Zeroconf()
-        ServiceBrowser(self.zc, service_type, handlers=[self.add_service])
-        print(f"Browsing for service type: {service_type}")
+        self.zc = AsyncZeroconf()
 
-    def find(self):
+    async def find(self):
+        AsyncServiceBrowser(self.zc.zeroconf, service_type, handlers=[self.add_service])
         while self.service_info is None:
-            time.sleep(1)
+            await asyncio.sleep(1)
             print(".", end="", flush=True)
         return socket.inet_ntoa(self.service_info.addresses[0]), self.service_info.port
 
@@ -60,11 +77,21 @@ class ServiceDiscover:
         name: str,
         state_change: ServiceStateChange,
     ) -> None:
-        print(f"Service Info: {self.service_info}")
-        info = zeroconf.get_service_info(service_type, name)
+        try:
+            asyncio.ensure_future(
+                self.async_display_service_info(zeroconf, service_type, name)
+            )
+        except Exception as e:
+            print(f"Error retrieving service info: {e}")
+
+    async def async_display_service_info(
+        self, zeroconf: Zeroconf, service_type: str, name: str
+    ):
+        info = AsyncServiceInfo(service_type, name)
+        await info.async_request(zeroconf, 3000)
         self.service_info = info
         print(
-            f"Service {name} added, address: {socket.inet_ntoa(info.addresses[0])}, port: {info.port}"
+            f"Added service {name} - address: {socket.inet_ntoa(info.addresses[0])}, port: {info.port}"
         )
 
     def __end__(self):
